f31aa5595e947dd94bfd8055dcaa54da95c88947
Wilmar Alexander Quintero Montoya <111884757+wilq0836@users.noreply.github.com>
2022-09-26 10:24:47 -0500

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,17 @@
+
+const modal = document.querySelector("#portafolio");
+
+function crearModal(){
+    Swal.fire({
+        title: 'Sweet!',
+        text: 'Modal with a custom image.',
+        imageUrl: 'https://unsplash.it/400/200',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+      })
+
+
+};
+
+
